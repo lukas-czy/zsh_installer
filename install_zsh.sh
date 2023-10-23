1106821239d19623116e9113d3892072afbc6810
Lukas Czycholl <56868169+lukas-czy@users.noreply.github.com>
2023-10-23 15:12:58 +0200

    added configs via upload

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,13 +3,22 @@ sudo apt install zsh -y
 zsh --version
 chsh -s $(which zsh)
 sudo apt-get install git-all -y
-# get all addons
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# put the config files in the correct places
-cp ./zshrc ~/.zshrc
-cp ./p10k.zsh ~/.p10k.zsh
-#start zsh
-zsh  
+
+
+echo '
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; 
+then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh' > ~/.zshrc
