3fb64fdff724a00f3be96b5d8bb42965bcf71fff
Lukas Czycholl <56868169+lukas-czy@users.noreply.github.com>
2023-10-23 15:13:26 +0200

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,7 +2,6 @@
 sudo apt install zsh -y
 zsh --version
 chsh -s $(which zsh)
-sudo apt-get install git-all -y
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
