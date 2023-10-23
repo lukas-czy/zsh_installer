276de9a0fec21809d8b6c3a3db4f1b6b589a2193
Lukas Czycholl <56868169+lukas-czy@users.noreply.github.com>
2023-10-23 15:31:03 +0200

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -7,4 +7,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+cp /tmp/zsh_installer/zshrc ~/.zshrc
+cp /tmp/zsh_installer/p10k.zsh ~/.p10k.zsh
+
+echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
+
 zsh
