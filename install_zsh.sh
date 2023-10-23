c6d35e50fa9245c800f1d133fe9f03a163446eb0
Lukas Czycholl <56868169+lukas-czy@users.noreply.github.com>
2023-10-23 16:22:58 +0200

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -5,12 +5,17 @@ zsh --version
 echo "installing oh-my-zsh"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 echo "getting addons"
+sudo rm -r ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sudo rm -r ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo rm -r ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "installing configurations"
-cp /tmp/zsh_installer/zshrc ~/.zshrc
-cp /tmp/zsh_installer/p10k.zsh ~/.p10k.zsh
+sudo rm ~/.zshrc
+sudo cp /tmp/zsh_installer/zshrc ~/.zshrc
+sudo rm ~/.p10k.zsh
+sudo cp /tmp/zsh_installer/p10k.zsh ~/.p10k.zsh
 
 echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
 echo "setting default shell to zsh"
